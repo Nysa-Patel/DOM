6666f958177d177f947808c73e43193a6b656b50
Nysa-Patel <nysapatel@gmail.com>
2025-03-14 09:09:33 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+let title = document.getElementById("header");
+title.onclick = () => {
+    let userinput = prompt("What is your name?");
+    title.innerHTML +=  + userinput;
+    window.open();
+};gi
+//<p> Hi + userinput</p>
+
+//for(i = 0; i<10000; i++)
+//title.innerHTML += "Good Bye!"
